8303c21a6a4f231c159e13e602407504e1635789
Peter Lollo <peteralollo@gmail.com>
2016-01-18 10:48:01 -0800

    Implement Grub class with pseudoclassical instantiaion pattern

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,11 @@
 var Grub = function() {
+	this._storage = {};
 };
+
+Grub.prototype.age = 0;
+Grub.prototype.color = 'pink';
+Grub.prototype.food = 'jelly';
+Grub.prototype.eat = function () {
+};
+
 
